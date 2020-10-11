e91679f574934f4fa637ba53a10f3077c3a44dfa
Joshua Immordino <joshimmor@Gmail.com>
2020-10-11 15:08:32 -0400

    about page link mobile

--- src/Components/About.js
+++ src/Components/About.js
@@ -22,7 +22,7 @@ function Bio(){
       <Col md="6" className="bio-col">
         <h4>I'm a Frontend Developer from New York, I have an extensive background in SEO, UX and User Research </h4>
       </Col>
-      <Col md=""  className="icon-row">
+      <Col  md="6"  className="d-none d-lg-flex icon-row">
                                 <animated.span style={iconAnimation} onClick={()=> window.open("https://github.com/Joshimmor/nucamp-react-native", "_blank")} className="icon-left"><FontAwesomeIcon   icon={faGit}/></animated.span>
                                 <animated.span style={iconAnimation} onClick={()=> window.open("mailto:joshimmor@gmail.com", "_blank")}><FontAwesomeIcon  icon={faEnvelope}/></animated.span>
                                 <animated.span style={iconAnimation} onClick={()=> window.open("https://dribbble.com/alienBlue", "_blank")} className="icon-right"><FontAwesomeIcon  icon={faDribbble}/></animated.span>
@@ -38,24 +38,55 @@ function Skills (){
       transform: "translateX(-1000px)"
     },
   })
+  const iconAnimation = useSpring({
+    transform:"translateX(300px)",
+    opacity:1,
+    color:"white",
+    config: { duration: 3000 },
+    from:{
+     transform:"translateX(0)",
+     opacity:0,
+    }
+  })
   return(
-      <Row className="skill-row"xs="12">
-          <Col xs="12"className="javascript-skill">
+      <Row className="skill-row" xs="12">
+        <Col xs="8" md="12">
+          <Row xs="12"  className="javascript-skill">
              <h4>Javascript</h4> 
+          </Row> 
+          <Row xs="12"  className="javascript-status-row">
              <animated.div style={load}  className="javascript-status-bar"/>
-          </Col> 
-          <Col xs="12"className="react-skill">
+          </Row> 
+          <Row xs="12"  className="react-skill">
              <h4>React</h4> 
+           </Row>
+          <Row xs="12"  className="react-status-row">
              <animated.div style={load} className="react-status-bar"/>
-          </Col> 
-          <Col xs="12"className="css-skill">
+          </Row> 
+          <Row xs="12"  className="css-skill">
              <h4>CSS</h4> 
+          </Row>
+          <Row xs="12"  className="css-status-row">
              <animated.div style={load} className="css-status-bar"/>
-          </Col> 
-          <Col xs="12"className="python-skill">
+          </Row> 
+          <Row xs="12"  className="python-skill">
              <h4>Python</h4> 
+          </Row>
+          <Row xs="12"  className="python-status-row">
              <animated.div style={load} className="python-status-bar"/>
-          </Col> 
+          </Row> 
+        </Col>
+        <Col xs="4" className="mobile-icon-box">
+          <Row xs="12" className="button-row-git">
+             <animated.span style={iconAnimation} onClick={()=> window.open("https://github.com/Joshimmor/nucamp-react-native", "_blank")} ><FontAwesomeIcon   icon={faGit}/></animated.span>
+          </Row> 
+          <Row xs="12" className="button-row-email">
+             <animated.span style={iconAnimation} onClick={()=> window.open("mailto:joshimmor@gmail.com", "_blank")}><FontAwesomeIcon  icon={faEnvelope}/></animated.span>
+          </Row> 
+          <Row xs="12" className="button-row-dribble">
+            <animated.span style={iconAnimation} onClick={()=> window.open("https://dribbble.com/alienBlue", "_blank")} ><FontAwesomeIcon  icon={faDribbble}/></animated.span>
+          </Row>
+        </Col>
         </Row>
  
        
